1e5abd1c30f8f7edb317843e6c225a046ad6e43a
MinKuk Jo <strike0115@naver.com>
2018-12-09 01:43:51 +0900

    Integration from minkuk

---
+++ INTEGRATION_GAMEJAM/Assets/Scripts/mamison.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mamison : InteractObject
+{
+    public AudioSource mamison_sound;
+
+	public override void InteractAction()
+	{
+        base.InteractAction();
+        mamison_sound = GetComponent<AudioSource>();
+        mamison_sound.Play();
+	}
+}
